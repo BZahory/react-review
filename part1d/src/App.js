1b961977877afce190f58e579acc4f2ba6e79c94
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-12 16:26:51 -0400

    1.11

--- part1d/src/App.js
+++ part1d/src/App.js
@@ -1,25 +1,24 @@
-import './App.css';
 import {useState} from 'react';
 
 const Feedback = ({rating, handleClick}) => {
   return <button onClick={handleClick} >{rating}</button>
 }
 
-const StatisticsLine = ({name, value}) => <p>{name} {value}</p>
+const StatisticsLine = ({name, value}) => <tr><td>{name}</td> <td>{value}</td></tr>
 
 const Statistics = ({good, bad, neutral}) => {
   const all = good+bad+neutral;
   const average = (sum, n) => sum / n;
   const percent = (n, d) => 100 * n / d;
 
-  return ([
-    <StatisticsLine name="good" value={good}/>,
-    <StatisticsLine name="neutral" value={neutral}/>,
-    <StatisticsLine name="bad" value={bad}/>,
-    <StatisticsLine name="all" value={all}/>,
-    <StatisticsLine name="average" value={average(good-bad, all)}/>,
-    <StatisticsLine name="percent" value={percent(good, all) + "%"}/>,
-  ])
+  return (<table>
+    <StatisticsLine name="good" value={good}/>
+    <StatisticsLine name="neutral" value={neutral}/>
+    <StatisticsLine name="bad" value={bad}/>
+    <StatisticsLine name="all" value={all}/>
+    <StatisticsLine name="average" value={average(good-bad, all)}/>
+    <StatisticsLine name="percent" value={percent(good, all) + "%"}/>
+  </table>)
 }
 
 function App() {
@@ -28,20 +27,18 @@ function App() {
   const [bad, setBad] = useState(0);
 
   return (
-    <div className="App">
+    <div>
     <h1>
       give feedback
     </h1>
-
-    <div>
     <Feedback handleClick={()=>setGood(good+1)} rating={"good"}/>
     <Feedback handleClick={()=>setNeutral(neutral+1)} rating={"neutral"}/>
     <Feedback handleClick={()=>setBad(bad+1)} rating={"bad"}/>
-    </div>
 
     <h1>statistics</h1>
-
+    <div >
     {good+neutral+bad>0 ? <Statistics good={good} bad={bad} neutral={neutral}/> : <p>No feedback given</p>}
+    </div>
 
     </div>
   );
