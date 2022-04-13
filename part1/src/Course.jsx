e89fb856befd492c90d78916fc8203e090adc8bc
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-13 18:49:17 -0400

    2.3

--- part1/src/Course.jsx
+++ part1/src/Course.jsx
@@ -13,6 +13,9 @@ export default function Course({course}) {
     <Part part={course.parts[0].name} exercises={course.parts[0].exercises} />,
     <Part part={course.parts[1].name} exercises={course.parts[1].exercises} />,
     <Part part={course.parts[2].name} exercises={course.parts[2].exercises} />,
-    <p style={{fontWeight:'bold'}}>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>,
+    <p style={{fontWeight:'bold'}}>Number of exercises {course.parts.reduce(
+        (previousValue=0, currentValue) => previousValue + currentValue.exercises,
+        0
+      )}</p>,
   ];
 }
