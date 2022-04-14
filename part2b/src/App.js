bd3ed329c40d354fed50c59dbbb5fedd934fc697
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-14 15:58:47 -0400

    2.10

--- part2b/src/App.js
+++ part2b/src/App.js
@@ -6,51 +6,75 @@ const App = () => {
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
 
-  let displayed = filter != "" ? persons.filter(person => person.name.includes(filter)) : persons;
+  let displayed =
+    filter != ""
+      ? persons.filter((person) => person.name.includes(filter))
+      : persons;
 
-  return (
-    <div>
-      <h2>Phonebook</h2>
+  const filterContacts = () => {
+    return (
       <form>
-        <div>
         <h1>Phonebook</h1>
         filter shown with
         <input
-            onChange={(event) => {
-              setFilter(event.target.value);
-            }}
-          />
-          <h1>add a new</h1>
-          name:
-          <input
-            onChange={(event) => {
-              setNewName(event.target.value);
-            }}
-          />
-          number:
-          <input
-            onChange={(event) => {
-              setNewNumber(event.target.value);
-            }}
-          />
-        </div>
-        <div>
-          <button
-            onClick={(event) => {
-              event.preventDefault();
-              persons.find(o => o.name === newName) == undefined
-                ? setPersons((p) => [...p, { name: newName, number: newNumber }])
-                : alert(`${newName} is already added to phonebook`);
-            }}
-            type="submit"
-          >
-            add
-          </button>
-        </div>
+          onChange={(event) => {
+            setFilter(event.target.value);
+          }}
+        />
       </form>
-      <h2>Numbers</h2>
-      {displayed.map((x) => (
-    <p key={x.name}>{x.name} {x.number}</p>))}
+    );
+  };
+
+  const addContact = () => {
+    return (
+      <form>
+        <h2>Phonebook</h2>
+        <h1>add a new</h1>
+        name:
+        <input
+          onChange={(event) => {
+            setNewName(event.target.value);
+          }}
+        />
+        number:
+        <input
+          onChange={(event) => {
+            setNewNumber(event.target.value);
+          }}
+        />
+        <button
+          onClick={(event) => {
+            event.preventDefault();
+            persons.find((o) => o.name === newName) == undefined
+              ? setPersons((p) => [...p, { name: newName, number: newNumber }])
+              : alert(`${newName} is already added to phonebook`);
+          }}
+          type="submit"
+        >
+          add
+        </button>
+      </form>
+    );
+  };
+
+  const displayContacts = () => {
+    return (
+      <div>
+        <h2>Numbers</h2>
+        {displayed.map((x) => (
+          <p key={x.name}>
+            {x.name} {x.number}
+          </p>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      {filterContacts()}
+      {addContact()}
+      {displayContacts()}
     </div>
   );
 };
