dc0a13773b14572953ac33589ab255077deb83fc
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-15 15:53:47 -0400

    2.12

--- part2c/src/App.js
+++ part2c/src/App.js
@@ -1,41 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const App = () => {
-  const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
-  const [showAll, setShowAll] = useState(true)
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    console.log('effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fulfilled')
-        setNotes(response.data)
-      })
-  }, [])
-  console.log('render', notes.length, 'notes')
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      console.log(response.data);
+      setCountries(response.data);
+    });
+  }, []);
+
+  let displayed =
+    filter != ""
+      ? countries.filter((countries) =>
+          countries.name.common.toLowerCase().includes(filter.toLowerCase())
+        )
+      : countries;
+
+  const filterCountries = () => {
+    return (
+      <form>
+        <h1>Phonebook</h1>
+        filter shown with
+        <input
+          onChange={(event) => {
+            setFilter(event.target.value);
+          }}
+        />
+      </form>
+    );
+  };
+
+  const displayCountries = () => {
+    return (
+      <div>
+        <h2>Numbers</h2>
+        {displayed.length < 10 && displayed.length > 1 ? (
+          displayed.map((x) => <p key={x.name.common}>{x.name.common}</p>)
+        ) : (
+          <div>
+            {displayed.length > 1 ? (
+              "Too many matches, specify another filter"
+            ) : displayed.length != 1 ? (
+              "No countries found"
+            ) : (
+              <div>
+                <h1>{displayed[0].name.common}</h1>
+                <p>capital {displayed[0].capital}</p>
+              <p>area {displayed[0].area}</p>
+              <h1>languages</h1>
+              <ul>
+              {Object.values(displayed[0].languages).map((x)=><li key={x}>{x}</li>)}
+              </ul>
+              <img src={displayed[0].flags.png} />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {filterCountries()}
+      {displayCountries()}
     </div>
   );
-}
+};
 
 export default App;
