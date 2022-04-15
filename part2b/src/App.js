3fa33a00a5dc724de1f0d3d68cebe0e20edb15c2
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-15 15:09:10 -0400

    2.11

--- part2b/src/App.js
+++ part2b/src/App.js
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons').then((response)=>{
+      console.log(response.data);
+      setPersons(response.data)});
+  }, [])
 
   let displayed =
     filter != ""
